dc9a991b51af6d3f9a9fa5c710b26043c312d019
Wert5 <Wert5@users.noreply.github.com>
2015-09-21 09:16:50 -0400

    Update MainChat.java

--- MainChat.java
+++ MainChat.java
@@ -1,13 +1,18 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 
 
-public class MainChat extends JFrame {
+public class MainChat extends JFrame implements ActionListener{
 	private String name="";
 	private String message="";
+	private SendPanel send = new SendPanel(this);
+	private MessagePanel mesPan = new MessagePanel(this);
 	
 	public MainChat(){
 		super();
-		
+		this.getContentPane().add(send);
 	}
 
 	public static void main(String[] args) {
@@ -15,6 +20,12 @@ public class MainChat extends JFrame {
 		MainChat screen= new MainChat();
 		screen.setVisible(true);
 		screen.setSize(500, 500);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
