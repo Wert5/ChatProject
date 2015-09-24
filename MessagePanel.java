e87444ba33ee36f3a5842455fa7be05ea2da175d
Wert5 <Wert5@users.noreply.github.com>
2015-09-24 09:14:14 -0400

    Update MessagePanel.java

--- MessagePanel.java
+++ MessagePanel.java
@@ -1,7 +1,25 @@
+import java.awt.*;
+
 import javax.swing.*;
 
 public class MessagePanel extends JPanel {
-	public MessagePanel(JFrame listen){
+	private JTextArea msg= new JTextArea("Help");
+	private JScrollPane scroll= new JScrollPane(msg);
+	private JButton exitBut= new JButton("Exit");
+	private Box b=Box.createVerticalBox();
+	public MessagePanel(MainChat listen){
 		super();
+		msg.setEditable(false);
+		this.add(b);
+		
+		//scroll.add(msg);
+		b.add(scroll);
+		b.add(exitBut);
+		exitBut.addActionListener(listen);
+	}
+	
+	public void setMsg(String txt){
+		msg.setText(txt);
+		repaint();
 	}
 }
